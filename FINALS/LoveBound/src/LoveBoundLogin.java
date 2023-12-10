3a3c9fd6551cd735f814b4b949ef23e41c302eb2
BrylDG <148348465+BrylDG@users.noreply.github.com>
2023-12-11 00:02:30 +0800

    Add files via upload

---
+++ FINALS/LoveBound/src/LoveBoundLogin.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.*;
+public class LoveBoundLogin {
+    public static void main(String[] args) throws IOException {
+        UserRegistrationSerialization registration = new UserRegistrationSerialization();
+        UserLogin login = new UserLogin();
+        int choice;
+        do {
+            System.out.println("Welcome to LoveBound, where dates matter.");
+            System.out.println("\n1.Login" + "\n2.Register" + "\n3.Exit");
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    login.main();
+                    choice = 3;
+                    break;
+                case 2:
+                    registration.main();
+                    break;
+                case 3:
+                    System.out.print("Exiting program!");
+                    break;
+                default:
+                    System.out.print("Enter valid choice");
+            }
+        }
+        while (choice != 3) ;
+    }
+}
